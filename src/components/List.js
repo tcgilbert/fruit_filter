48730dc206d5cad2dcb437cba90bf114f56322d6
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-10 11:41:42 -0500

    created list component

---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import React, { Componeny } from 'react';
+
+class List extends Component {
+
+  constructor(props) {
+    super()
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>List</h1>
+      </div>
+    )
+  }
+}
+
+export default List;
